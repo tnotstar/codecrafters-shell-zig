11e0b33e16c260049c5a73e67d03734f4868e960
Antonio Alvarado <tnotstar@gmail.com>
2026-05-05 01:14:57 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
+const BuiltinCommands = enum {
+    exit,
+    echo,
+    type,
+};
+
 pub fn main(init: std.process.Init) !void {
     var stdio_buffer: [4096]u8 = undefined;
     var stdin = std.Io.File.stdin().reader(init.io, &stdio_buffer);
@@ -8,16 +14,35 @@ pub fn main(init: std.process.Init) !void {
     while (true) {
         try stdout.interface.print("$ ", .{});
 
-        const commandLine = try stdin.interface.takeDelimiter('\n');
-        if (std.mem.eql(u8, commandLine.?, "exit")) {
-            break;
-        }
+        const cmdLine = try stdin.interface.takeDelimiter('\n');
+        var it = std.mem.tokenizeAny(u8, cmdLine.?, " ");
 
-        if (std.mem.startsWith(u8, commandLine.?, "echo ")) {
-            try stdout.interface.print("{s}\n", .{commandLine.?[5..]});
-            continue;
-        }
+        const command = it.next() orelse continue;
+        if (std.meta.stringToEnum(BuiltinCommands, command)) |builtin| {
+            switch (builtin) {
+                .exit => break,
 
-        try stdout.interface.print("{s}: command not found\n", .{commandLine.?});
+                .echo => {
+                    const rest = it.rest();
+                    try stdout.interface.print("{s}\n", .{rest});
+                    continue;
+                },
+
+                .type => {
+                    const identifier = it.next() orelse {
+                        try stdout.interface.print("type: missing identifier\n", .{});
+                        continue;
+                    };
+
+                    if (std.meta.stringToEnum(BuiltinCommands, identifier)) |_| {
+                        try stdout.interface.print("{s} is a shell builtin\n", .{identifier});
+                    } else {
+                        try stdout.interface.print("{s}: not found\n", .{identifier});
+                    }
+                },
+            }
+        } else {
+            try stdout.interface.print("{s}: command not found\n", .{cmdLine.?});
+        }
     }
 }
