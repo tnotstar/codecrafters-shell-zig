e2e0d1169f188eefced3f752659130e4a4d54b5a
Antonio Alvarado <tnotstar@gmail.com>
2026-05-04 11:07:02 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
+    var stdio_buffer: [4096]u8 = undefined;
+    var stdin = std.Io.File.stdin().reader(init.io, &stdio_buffer);
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
 
-    // TODO: Uncomment the code below to pass the first stage
     try stdout.interface.print("$ ", .{});
+    const command = try stdin.interface.takeDelimiter('\n');
+    try stdout.interface.print("{s}: command not found\n", .{command.?});
 }
